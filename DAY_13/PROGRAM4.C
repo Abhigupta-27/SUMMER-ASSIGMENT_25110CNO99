805922132b5d46bc1b477e49140efd5ed7cd5622
Abhigupta-27 <abhigupta1363@gmail.com>
2026-06-14 22:13:56 +0530

    Add files via upload

---
+++ DAY_13/PROGRAM4.C
@@ -0,0 +1,23 @@
+//Write a Program to Count Even and Odd Elements
+#include <stdio.h>
+int main()
+{
+    int arr[100], n, i, even = 0, odd = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+    printf("Even Elements = %d\n", even);
+    printf("Odd Elements = %d", odd);
+    return 0;
+}
