a6c6862a84a0b866833a7216f60e83853f256fab
Abhigupta-27 <abhigupta1363@gmail.com>
2026-06-17 13:38:28 +0530

    Add files via upload

---
+++ DAY_17/PROGRAM2.C
@@ -0,0 +1,34 @@
+//Write a Program for Union of Two Arrays
+#include <stdio.h>
+int main(){
+    int a[50], b[50], c[100];
+    int n1, n2, i, j, k = 0, flag;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    for(i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    for(i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+    for(i = 0; i < n1; i++)
+        c[k++] = a[i];
+    for(i = 0; i < n2; i++)
+    {
+        flag = 0;
+        for(j = 0; j < k; j++)
+        {
+            if(b[i] == c[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+            c[k++] = b[i];
+    }
+    printf("Union Array:\n");
+    for(i = 0; i < k; i++)
+        printf("%d ", c[i]);
+    return 0;
+}
