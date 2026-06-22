e83b17a7d99de588c288b735c9140a45657481ac
Abhigupta-27 <abhigupta1363@gmail.com>
2026-06-22 13:15:33 +0530

    Add files via upload

---
+++ DAY_22/program4.c
@@ -0,0 +1,20 @@
+//Write a program to Remove spaces from string
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i = 0, j = 0;
+    printf("Enter a string: ");
+    gets(str);
+    while(str[i] != '\0')
+    {
+        if(str[i] != ' ')
+        {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    str[j] = '\0';
+    printf("String after removing spaces = %s", str);
+    return 0;
+}
